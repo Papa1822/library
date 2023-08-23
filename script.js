8b1b15df3dbd9dfa1668790c6593e6c7f22172d9
Papa1822 <mikepaps1822000@gmail.com>
2023-08-23 12:46:43 +0000

    Added a newBookbtn function to make the form appear on screen when the new book button is clicked.

---
+++ script.js
@@ -0,0 +1,20 @@
+let myLibrary = [];
+
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+};
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function(){
+
+    let newBookForm = document.querySelector("#new-book-form");
+   newBookForm.style.display = "block";
+    
+})
+
+
